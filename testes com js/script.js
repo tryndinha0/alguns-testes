6fc525cb6478b3ac209335935ea9529c51566cd4
tryndinha0 <120797506+tryndinha0@users.noreply.github.com>
2023-12-12 18:11:40 -0300

    mais estudo

---
+++ testes com js/script.js
@@ -0,0 +1,13 @@
+
+function aleatorio(){
+    var res = document.querySelector('p#res')
+    var numero = Math.round(Math.random(numero)*10)
+    res.innerHTML = numero
+    if(numero % 2 == 0 ){
+        document.body.style.backgroundColor = 'white'
+    }else{
+        document.body.style.backgroundColor = 'red'
+    }
+   
+}
+
